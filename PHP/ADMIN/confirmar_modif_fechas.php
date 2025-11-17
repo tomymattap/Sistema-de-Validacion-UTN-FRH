7b70a79f46436df98971a3ece5b77b5e08501f9f
Tomás Matta Palladino <tomasmattap@gmail.com>
2025-11-17 10:43:11 -0300

    Update confirmar_modif_fechas.php

--- PHP/ADMIN/confirmar_modif_fechas.php
+++ PHP/ADMIN/confirmar_modif_fechas.php
@@ -128,7 +128,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cursos'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmación de Cambios - Admin</title>
-    <link rel="icon" href="../Imagenes/icon.png" type="image/png">
+    <link rel="icon" href="../../Imagenes/icon.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../../CSS/general.css">
     <link rel="stylesheet" href="../../CSS/ADMIN/gestionar_cursos.css">
